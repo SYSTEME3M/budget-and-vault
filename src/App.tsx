bc8ce7b4fdd4676fd394494710e101fa9fa4bc3d
SYSTEME3M <erickpapko384@gmail.com>
2026-03-18 09:41:29 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,10 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+    <BrowserRouter>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<Navigate to="/login" replace />} />
@@ -38,8 +38,8 @@ const App = () => (
           <Route path="/profil" element={<AuthGuard><ProfilPage /></AuthGuard>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+      </TooltipProvider>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
