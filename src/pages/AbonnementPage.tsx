399d84c4838cbea6aff871994ef981fea87a19d6
SYSTEME3M <erickpapko384@gmail.com>
2026-03-21 11:17:54 +0100

    Add 'use client' directive to AbonnementPage

--- src/pages/AbonnementPage.tsx
+++ src/pages/AbonnementPage.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import AppLayout from "@/components/AppLayout";
 import { getNexoraUser, isNexoraAdmin, hasNexoraPremium } from "@/lib/nexora-auth";
